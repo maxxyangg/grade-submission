584d2a182cf92c20a3013501b3e1542b4478195f
Maximillian Yang <maxxyangg@gmail.com>
2023-07-14 00:30:02 -0500

    Initial

---
+++ src/main/java/com/example/gradesubmission/entity/Grade.java
@@ -0,0 +1,16 @@
+package com.example.gradesubmission.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Grade {
+
+    private Long id;
+    private String score;
+    private Student student;
+    private Course course;
+
+    
+}
---
+++ src/main/java/com/example/gradesubmission/entity/User.java
@@ -0,0 +1,5 @@
+package com.example.gradesubmission.entity;
+
+public class User {
+    
+}
